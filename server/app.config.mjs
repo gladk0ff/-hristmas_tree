f58744c78fa1f684bcaecdad7976a7c1369ac241
gladkoff <agladkov@logrocon.com>
2023-06-23 23:48:15 +0300

    fix

--- server/app.config.mjs
+++ server/app.config.mjs
@@ -1,3 +1,4 @@
-module.exports = {
-    PORT: process.env.API_PORT || 3000
+export default {
+    PORT: process.env.API_PORT || 3000,
+    ME_CONFIG_MONGODB_URL: process.env.ME_CONFIG_MONGODB_URL || 'mongodb://localhost:27017'
 }
