8dafd8bb2bcda67dd3d5af50cde0533faeb087f5
gladk0ff <sergladkov@outlook.com>
2023-12-03 20:11:21 +0300

    исправлен тултип и закрытие формы

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -1,73 +1,65 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-import Snow from 'react-snow-effect';
-import Toy from '@components/Toy/Toy';
+import Snow from "react-snow-effect";
+import Toy from "@components/Toy/Toy";
 import NewToy from "@components/NewToy";
-import treeImg from '@assets/pngegg.png';
-import logo from '@assets/logo.svg'
-import Background2 from '@assets/background.jpg'
+import treeImg from "@assets/pngegg.png";
+import logo from "@assets/logo.svg";
+import Background2 from "@assets/background.jpg";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-    const [toys, updateToys] = useState([...new Array(1)]);
-    console.log(" process.env.CURRENT_YEAR", process.env.CURRENT_YEAR);
-    
-    const [ourToys, updateOurToys] = useState([]);
-    const [size,setSize]=useState({
-        clientHeight:  document.getElementById("app")?.clientHeight||0,
-        clientWidth: document.getElementById("app")?.clientWidth||0
-    })
-    
-    const tree = useRef(null);
+	const [ourToys, updateOurToys] = useState([]);
+	const [size, setSize] = useState({
+		clientHeight: document.getElementById("app")?.clientHeight || 0,
+		clientWidth: document.getElementById("app")?.clientWidth || 0,
+	});
 
-    const getToys = () =>{
-        fetch(`${process.env.API_SERVICE_URL}/toys`)
-            .then(resp=>{
-                return resp.json()
-            })
-            .then(resp=>{
-                console.log("getToys resp",resp)
-                if (resp.length) updateOurToys(resp)
-                else throw new Error({message:resp})
-        }).catch(e=>{
-            console.log('error',e.toString())
-        })
-    }
+	const tree = useRef(null);
 
-    const changeSize = ()=>{
-        setSize({
-            clientHeight:  document.getElementById("app")?.clientHeight||0,
-            clientWidth: document.getElementById("app")?.clientWidth||0
-        })
-    }
+	const getToys = () => {
+		fetch(`${process.env.API_SERVICE_URL}/toys`)
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((resp) => {
+				console.log("getToys resp", resp);
+				if (resp.length) updateOurToys(resp);
+				else throw new Error({ message: resp });
+			})
+			.catch((e) => {
+				console.log("error", e.toString());
+			});
+	};
 
-    useEffect(() => {
-        getToys();
-        window.addEventListener('resize',changeSize);
-        return()=>{
-            window.removeEventListener('resize',changeSize)
-        }
-    }, [])
+	const changeSize = () => {
+		setSize({
+			clientHeight: document.getElementById("app")?.clientHeight || 0,
+			clientWidth: document.getElementById("app")?.clientWidth || 0,
+		});
+	};
 
-    return (
-        <div className="christmas-tree-app scene"
-           style={{backgroundImage:`url(${Background2})`}}
-        >
-            <h1 className='christmas-tree-app__current-year'>{ process.env.CURRENT_YEAR}</h1>
-            <Snow/>
-            <NewToy target={tree} clientSize={size} />
-            {ourToys?.map((toy, index) => <Toy clientSize={size} toy={toy} key={"toy" + index}/>)}
-            <main className="christmas-tree-app__tree-container ">
-                <img src={treeImg} ref={tree} title="Елка" className="droppable"/>
-            </main>
-            <img className="christmas-tree-app__footer-logo" src={logo} alt=""/>
-        </div>
-    );
+	useEffect(() => {
+		getToys();
+		window.addEventListener("resize", changeSize);
+		return () => {
+			window.removeEventListener("resize", changeSize);
+		};
+	}, []);
+
+	return (
+		<div className="christmas-tree-app scene" style={{ backgroundImage: `url(${Background2})` }}>
+			<h1 className="christmas-tree-app__current-year">{process.env.CURRENT_YEAR}</h1>
+			<Snow />
+			<NewToy target={tree} clientSize={size} />
+			{ourToys?.map((toy, index) => <Toy clientSize={size} toy={toy} key={"toy" + index} />)}
+			<main className="christmas-tree-app__tree-container ">
+				<img src={treeImg} ref={tree} title="Елка" className="droppable" />
+			</main>
+			<img className="christmas-tree-app__footer-logo" src={logo} alt="" />
+		</div>
+	);
 }
 
 export default App;
-
-
-
-
--- front/src/components/Toy/Toy.tsx
+++ front/src/components/Toy/Toy.tsx
@@ -1,40 +1,46 @@
-import React, {MutableRefObject, useEffect, useRef, useState} from 'react';
-import Tippy from '@tippyjs/react';
-import T1 from '@assets/toy1.png';
-import T2 from '@assets/toy2.png';
-import T3 from '@assets/toy3.png';
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import Tippy from "@tippyjs/react/headless";
+import T1 from "@assets/toy1.png";
+import T2 from "@assets/toy2.png";
+import T3 from "@assets/toy3.png";
 
-import './Toy.css';
+import "./Toy.css";
 
 export const getRandomInt = (max) => {
-    const val = Math.floor(Math.random() * max);
-    return (val > 0) ? val : 1;
-}
+	const val = Math.floor(Math.random() * max);
+	return val > 0 ? val : 1;
+};
 
 export const config = {
-    1: T1,
-    2: T2,
-    3: T3
-}
+	1: T1,
+	2: T2,
+	3: T3,
+};
 
-const Toy = ({ toy,clientSize}: any) => {
-    const { positionX, positionY, message } = toy
-    const {clientHeight,clientWidth } = clientSize
-    if (!positionX || !positionY || !message || !clientHeight || !clientWidth) return null;
-    
-    const style = {
-        left: clientWidth * positionX,
-        top: clientHeight * positionY,
-    };
+const Toy = ({ toy, clientSize }: any) => {
+	const { positionX, positionY, message } = toy;
+	const { clientHeight, clientWidth } = clientSize;
+	if (!positionX || !positionY || !message || !clientHeight || !clientWidth) return null;
 
-    return <Tippy content={message}>
-        <div
-             style={style}
-            className="toy toy--read-only">
-            <img src={config[getRandomInt(3)]}/>
-        </div>
-    </Tippy>
-    
-}
+	const style = {
+		left: clientWidth * positionX,
+		top: clientHeight * positionY,
+	};
 
-export default Toy
+	return (
+		<Tippy
+			// visible
+			render={(attrs) => (
+				<div className="tippy-container" {...attrs}>
+					<span className="tippy-messages">{message}</span>
+				</div>
+			)}
+		>
+			<div style={style} className="toy toy--read-only">
+				<img src={config[getRandomInt(3)]} className="toy__img" />
+			</div>
+		</Tippy>
+	);
+};
+
+export default Toy;
